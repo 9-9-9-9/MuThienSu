c3b97f25207a4d4bad5d1fe4fe8357d6bcdfffbe
HungPV <hungpv@goquo.com>
2020-04-10 23:00:23 +0700

    update

---
+++ MUThienSu/Exceptions/InvalidCommandArgumentsException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MUThienSu.CommandHandlers;
+
+namespace MUThienSu.Exceptions
+{
+    public class InvalidCommandArgumentsException : Exception
+    {
+        public InvalidCommandArgumentsException(ICommandHandler commandHandler, string msg)
+        : base($"Sai câu lệnh: .\\{System.AppDomain.CurrentDomain.FriendlyName} <account> <character> {commandHandler.RegisteredCommandName()} {msg}")
+        {
+        }
+    }
+
+    public static class InvalidCommandArgumentsExceptionExtensions
+    {
+        public static void ThrowInvalidCommandArgumentsException(
+            this ICommandHandler commandHandler, string msg = null)
+        {
+            throw new InvalidCommandArgumentsException(commandHandler, msg ?? string.Empty);
+        }
+    }
+}
